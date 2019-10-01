fd0008323340d80c201a18754bbd1943e491645e
Ilari Marjamaa <t7mail00@students.oamk.fi>
2019-10-01 17:11:03 +0300

    Initial Commit

---
+++ 6_1Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  6_1Lab
+//
+//  Created by Ilari Marjamaa on 01/10/2019.
+//  Copyright © 2019 Ilari Marjamaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
